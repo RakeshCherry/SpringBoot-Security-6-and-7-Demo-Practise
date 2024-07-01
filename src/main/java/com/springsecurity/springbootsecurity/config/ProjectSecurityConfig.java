0e5bcffd91cbe92a306476f4c9794d20cf2e9543
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-01 18:30:39 +0530

    learnt InMemoryUserDetailsManager

--- src/main/java/com/springsecurity/springbootsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/springbootsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,11 @@ package com.springsecurity.springbootsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,4 +25,49 @@ public class ProjectSecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
-}
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+
+
+        /* Approach 1 where we use withDefaultPasswordEncoder() method while creating the user details */
+
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("1234")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("1234")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+
+
+        /* Approach 2 we use NoOpPasswordEncoder Bean while creating the user details */
+
+        UserDetails admin = User.withUsername("admin")
+                .password("1234")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("1234")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+        }
+
+        /**
+         * NoOpPasswordEncoder is not recommended for production usage
+         * Use only for non-prod.
+         *
+         * @return PasswordEncoder
+         */
+
+        @Bean
+        public PasswordEncoder  passwordEncoder(){
+            return NoOpPasswordEncoder.getInstance();
+        }
+
+    }
